c1a2522987cf0762f089364aa41789453d5419ae
Zachary Danz <zsd4yr@virginia.edu>
2017-11-15 17:00:49 -0500

    naive init, turn back on of command line args, just use the bash script H

--- machine_learning.py
+++ machine_learning.py
@@ -3,7 +3,6 @@ from bitarray import bitarray
 import sys as sys
 
 #internal
-from jsonReader import decode
 from recipe import Recipe
 from setup import get_ingredients, get_recipes
 
@@ -11,17 +10,19 @@ def run_program(name_of_atr_file, name_of_training_file):
     attribute_list, attribute_to_index_dict = get_ingredients(name_of_atr_file)
     training_set = get_recipes(name_of_training_file, attribute_to_index_dict)
 
-    print(training_set)
-    print(training_set[0])
+    #print(training_set)
+    print(str(training_set[0]) + '\n')
+    print(str(training_set[0].ingredients[0]))
+    print(str(training_set[0].ingredients[6]))
     #next code here
 
 
 
-'''
+
 if __name__ == '__main__':
     if(len(sys.argv) > 2):
         #print(str(sys.argv))
         run_program(sys.argv[1], sys.argv[2])
-'''
 
-run_program("ingredients.json", "training.json")
+
+#run_program("ingredients.json", "training.json")
